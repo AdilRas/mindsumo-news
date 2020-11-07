a2404e380b14a60b321867dbd966f8daa0fa14fc
Adil Rasiyani <adil.rasiyani@tamu.edu>
2020-11-06 22:48:51 -0600

    Fix indentation

--- src/components/util/Checkbox/Checkbox.tsx
+++ src/components/util/Checkbox/Checkbox.tsx
@@ -1,23 +1,26 @@
 import React, { useState } from 'react'
 
 const Checkbox = (props: {text: string, handleToggle: (event: any)=>void}) => {
+
     const [checked, setChecked] = useState<boolean>(true);
+    
     const toggle = () => {
         setChecked(!checked);
     }
+    
     return (
         <div>
             <div className="custom-control custom-checkbox mb-3">
-            <input
-                className="custom-control-input"
-                id={`customCheck${props.text}`}
-                type="checkbox"
-                onChange={(event: any) => {toggle(); props.handleToggle(event); }}
-                checked={checked}
-            />
-            <label className="custom-control-label" htmlFor={`customCheck${props.text}`}>
-                {props.text}
-            </label>
+                <input
+                    className="custom-control-input"
+                    id={`customCheck${props.text}`}
+                    type="checkbox"
+                    onChange={(event: any) => {toggle(); props.handleToggle(event); }}
+                    checked={checked}
+                />
+                <label className="custom-control-label" htmlFor={`customCheck${props.text}`}>
+                    {props.text}
+                </label>
             </div>
         </div>
     )
